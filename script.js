35138ed2114512a09093b9c1867a294f544faf67
SHxBHAM <shubhammishra41880@gmail.com>
2024-01-02 23:03:10 +0530

    added functionality to the ghost button also added the 2nd page stuff.

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let videocontainer = document.querySelector('#video')
+let videocontainer = document.querySelector('.media')
     let playbtn = document.querySelector('#play-btn')
     let audioArray = ['./Seedhe Maut - _101_.mp3', "./Seedhe Maut - 'Nanchaku' ft MC STΔN LYRICS.mp3", "./Seedhe Maut - Khatta Flow  (Lyrics) ft. Kr$na  Lunch break (mixtape).mp3",]
     let audioIndex = 0;
+    let audioPlaying = false;
+    
     let playing;
 
     function sequentialplaying() {
         if (audioIndex < audioArray.length) {
-            playing = new Audio(audioArray[audioIndex]);
-            playing.play();
+            if(audioPlaying===false){
+                playing = new Audio(audioArray[audioIndex]);
+                playing.play();
+                audioPlaying=true;
+            }
+            else{
+                playing.pause()
+                audioPlaying=false;
+            }
+            
         }
     }
 
@@ -33,8 +43,15 @@ let videocontainer = document.querySelector('#video')
     });
 
     videocontainer.addEventListener("click", function () {
+        if(audioPlaying === false){
+            playbtn.innerHTML = `PAUSE`
+        }
+        else{
+            playbtn.innerHTML = `PLAY`
+        }
         sequentialplaying();
         playing.addEventListener("ended", function () {
+            audioPlaying = false;
             audioIndex++;
             sequentialplaying();
         });
